622b7976a9eef36a1ff12a41dd57139fcb1a41b9
RY0502 <ravi_yadav0502@yahoo.in>
2026-02-09 16:48:05 +0530

    Update main.js

--- src/main.js
+++ src/main.js
@@ -116,10 +116,10 @@ export default async function ({ req, res }) {
 
   const apiCalls = [
     // callOpenRouter(prompt, 'mistralai/mistral-7b-instruct:free'),
-    // callOpenRouter(prompt, 'openai/gpt-oss-20b:free'),
     //callOpenRouter(prompt, 'qwen/qwen-2.5-vl-7b-instruct:free'),
-    // callOpenRouter(prompt, 'meta-llama/llama-3.3-8b-instruct:free'),
      callOpenRouter(prompt, 'nvidia/nemotron-3-nano-30b-a3b:free'),
+      callOpenRouter(prompt, 'openai/gpt-oss-20b:free'),
+    callOpenRouter(prompt, 'meta-llama/llama-3.3-8b-instruct:free'),
   ];
 
   const results = await Promise.all(apiCalls);
